6b92bbd99fc289bd5995f498c0e260f503cc1552
Nazrulislam07 <34220598+Nazrulislam07@users.noreply.github.com>
2020-09-21 13:21:44 -0400

    Add files via upload

---
+++ JS Stalker exercise/stalker.js
@@ -0,0 +1,12 @@
+var firstName = prompt("What is your firstName?")
+var	lastName = prompt("What is your lastName?")
+var age = prompt("How old are you?")
+
+/*var fullName = firstName + " " + lastName;
+
+
+console.log("Your full name is " + fullName );
+console.log("You are " + age + " years old");*/
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
